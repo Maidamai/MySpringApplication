6b2568567613b5c2b59bdc7fce154fab97452dcf
Maidamai <caijiangtao521@foxmail.com>
2020-02-24 13:47:56 +0800

    初始化

---
+++ src/main/java/com/cai/myappweb/MyappWebApplication.java
@@ -0,0 +1,13 @@
+package com.cai.myappweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyappWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyappWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cai/myappweb/MyappWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cai.myappweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
